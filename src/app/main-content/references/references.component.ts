6a2ac90cd7ad6e45673a79404ad6c9ca15657c0a
Pirasanth Shanmuganathan <pirasanth@live.de>
2025-07-29 23:46:51 +0200

    content fix

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -18,24 +18,24 @@ export class ReferencesComponent {
       name: 'V. Schuster',
       role: 'Team Partner',
       quote: {
-        EN: `Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.`,
-        DE: `Michael hat unser Team mit seiner Organisation und klaren Kommunikation zusammengehalten. Ohne sein Engagement wären wir nicht so weit gekommen.`
+        EN: `Pirasanth really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.`,
+        DE: `Pirasanth hat unser Team mit seiner Organisation und klaren Kommunikation zusammengehalten. Ohne sein Engagement wären wir nicht so weit gekommen.`
       }
     },
     {
       name: 'E. Eichinger',
       role: 'Team Partner',
       quote: {
-        EN: `Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.`,
-        DE: `Michi war ein großartiger Teamkollege bei DA. Sein Engagement und seine Bereitschaft, Verantwortung zu übernehmen, haben wesentlich zum Erfolg beigetragen.`
+        EN: `Pirasanth was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.`,
+        DE: `Pirasanth war ein großartiger Teamkollege bei DA. Sein Engagement und seine Bereitschaft, Verantwortung zu übernehmen, haben wesentlich zum Erfolg beigetragen.`
       }
     },
     {
       name: 'I. Nuber',
       role: 'Frontend Engineer',
       quote: {
-        EN: `It was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.`,
-        DE: `Die Zusammenarbeit mit Michael war eine große Freude. Er versteht es, Teammitglieder zu motivieren und inspiriert durch kreative Beiträge. Besonders hervorzuheben ist sein Gespür für das Teamwohl – er ist immer präsent, hilfsbereit und hat dabei auch noch Humor.`
+        EN: `It was a great pleasure to work with Pirasanth. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.`,
+        DE: `Die Zusammenarbeit mit Pirasanth war eine große Freude. Er versteht es, Teammitglieder zu motivieren und inspiriert durch kreative Beiträge. Besonders hervorzuheben ist sein Gespür für das Teamwohl – er ist immer präsent, hilfsbereit und hat dabei auch noch Humor.`
       }
     }
   ];
